48b7e324e720cda91565789e3ed6394cb324d744
cybera1 <82648439+cybera1@users.noreply.github.com>
2022-07-25 21:52:40 +0530

    Add files via upload

---
+++ JS/app.js
@@ -0,0 +1,9 @@
+
+
+
+const mobile_nav = document.getElementById("mobile_nav"),
+hambarger = document.querySelector(".hambarger");
+
+hambarger.addEventListener("click", () => {
+mobile_nav.classList.toggle("open");
+});
